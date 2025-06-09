a9764e8efded84f27bf088bace24790b6d3062b4
rakhaa142 <rakhaaziz01010@gmail.com>
2025-06-09 10:43:53 -1200

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,15 @@
+// KhodamScript
+
+// Fungsi untuk memanggil khodam berdasarkan nama
+fungsi panggilKhodam(nama) {
+    cetak("Mengundang khodam bernama " + nama);
+}
+
+// Fungsi untuk memberikan perintah kepada khodam
+fungsi perintahKhodam(khodam, perintah) {
+    cetak("Memberikan perintah kepada khodam " + khodam + ": " + perintah);
+}
+
+// Contoh penggunaan
+panggilKhodam("Raja Jin");
+perintahKhodam("Raja Jin", "Bantulah saya dalam mencapai kesuksesan!");
